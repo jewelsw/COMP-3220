e4a3d2d6a6790ec2896597c5f75e8644a5f1eea1
jewels <157740546+jewelsw@users.noreply.github.com>
2024-09-27 21:43:22 +0000

    trying to troubleshoot my output

--- Ruby Lexer and Parser/TinyParser.rb
+++ Ruby Lexer and Parser/TinyParser.rb
@@ -4,8 +4,6 @@
 load "TinyToken.rb"
 load "TinyLexer.rb"
 class Parser < Lexer
-	errorCount = 0
-
 	def initialize(filename)
     	super(filename)
     	consume()
@@ -21,9 +19,9 @@ class Parser < Lexer
 	def match(dtype)
       	if (@lookahead.type != dtype)
          	puts "Expected #{dtype} found #{@lookahead.text}"
-		else
-			puts "Matched #{@lookahead.text}"
-		end
+        else 
+            puts "Found #{@lookahead.type} Token: #{@lookahead.text}"
+      	end
       	consume()
    	end
    	
@@ -44,106 +42,100 @@ class Parser < Lexer
 			puts "Entering ASSGN Rule"
 			assign()
 		end
+		
 		puts "Exiting STMT Rule"
 	end
-
-	#Parsing assignments
+    
     def assign()
-		if (@lookahead != nil)
-			id()
-			match(Token::ASSGN)
-			puts "Entering EXP Rule"
-			exp()
-		end
-		puts "Exiting ASSGN Rule"
-    end
-
-    #Parsing expressions
-    def exp() 
-		if (@lookahead != nil)
-			puts "Entering TERM Rule"
-        	term() 
-			puts "Entering ETAIL Rule"
-			etail()
-		end
-		puts "Exiting EXP Rule"
-    end
-
-	#Parsing terms
-	def term()
-		if (@lookahead != nil)
-			puts "Entering FACTOR Rule"
-			factor()
-			puts "Entering TTAIL Rule"
-			ttail()
-		end
-		puts "Exiting TERM Rule"
-	end
-
-	#Parsing right and left parenthesis
-	def factor()
-		if (@lookahead.type == Token::LPAREN)
-			match(Token::LPAREN)
-			puts "Entering EXP Rule"
-			exp()
-			match(Token::RPAREN)
-		elsif
-			int()
-		else
-			id()
-		end
-		puts "Exiting FACTOR Rule"
-	end
-
-	#Parsing additon and subtraction
-	def etail()
-		if (@lookahead.type == Token::ADDOP)
-			match(Token::ADDOP)
-			puts "Entering TERM Rule"
-			term()
-			puts "Entering ETAIL Rule"
-			etail()
-		elsif (@lookahead.type == Token::SUBOP)
-			match(Token::SUBOP)
-			puts "Entering TERM Rule"
-			term()
-			puts "Entering ETAIL Rule"
-			etail()
-		else
-			return
-		end
-	end
-
-	#Parsing multiplication and division
-	def ttail()
-		if (@lookahead.type == Token::MULTOP)
-			match(Token::MULTOP)
-			puts "Entering FACTOR Rule"
-			factor()
-			puts "Entering TTAIL Rule"
-			ttail()
-		elsif (@lookahead.type == Token::DIVOP)
-			match(Token::DIVOP)
-			puts "Entering FACTOR Rule"
-			factor()
-			puts "Entering TTAIL Rule"
-			ttail()
-		else
-			return
-		end
-	end
-
-	#'Parsing' identifiers
-    def id()
-		#puts "Found ID Token: #{@lookahead}"
-		match(Token::ID)
-    end
-
-    #'Parsing' integers
-    def int()
-		puts "Found INT Token: #{@lookahead}"
-		match(Token::INT)
-    end
-
-	puts "Errors found: #{errorCount}"
+        if (@lookahead != nil)
+            id()
+            match(Token::ASSGN)
+            puts "Entering EXP Rule"
+            exp()
+        end
+        puts "Exiting ASSGN Rule"
+     end
+                    
+     def exp()
+         if (@lookahead != nil)
+             puts "Entering TERM Rule"
+             term()
+             puts "Entering ETAIL Rule"
+             etail()
+         end
+         puts "Exiting EXP Rule"
+     end
+                    
+     def term()
+         if (@lookahead != nil)
+             puts "Entering FACTOR Rule"
+             factor()
+             puts "Entering TTAIL Rule"
+             ttail()
+         end
+         puts "Exitng TERM Rule"
+      end
+                  
+      def factor()
+          if (@lookahead.type == Token::LPAREN)
+              match(Token::LPAREN)
+              puts "Entering EXP Rule"
+              exp()
+              match(Token::RPAREN)
+          elsif
+              int()
+          else
+              id()
+          end
+          puts "Exiting FACTOR Rule"
+      end
+                
+      def etail()
+          if (@lookahead.type == Token::ADDOP)
+              match(Token::ADDOP)
+              puts "Entering TERM Rule"
+              term()
+              puts "Entering ETAIL Rule"
+              etail()
+          elsif (@lookahead.type == Token::SUBOP)
+              match(Token::SUBOP)
+              puts "Entering TERM Rule"
+              term()
+              puts "Entering ETAIL Rule"
+              etail()
+          else
+              puts "Did not find ADDOP of SUBOP Token, choosing EPSILON production"
+              return
+          end
+          puts "Exiting ETAIL Rule"
+       end
+                    
+       def ttail()
+           if (@lookahead.type == Token::MULTOP)
+               match(Token::MULTOP)
+               puts "Entering FACTOR Rule"
+               factor()
+               puts "Entering TTAIL Rule"
+               ttail()
+           elsif (@lookahead.type == Token::DIVOP)
+               match(Token::DIVOP)
+               puts "Entering FACTOR Rule"
+               factor()
+               puts "Entering TTAIL Rule"
+               ttail()              
+           else
+               puts "did not find MULTOP of DIVOP Token, choosing EPSILON production"
+               return
+           end
+           puts "Exiting TTAIL Rule"
+       end
+                  
+       def id()
+           match(Token::ID)
+       end
+                    
+       def int()
+           match(Token::INT)
+       end
+               
 end
